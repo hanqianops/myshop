259074f25c29b0af2e882baab3b9327be23bf1b6
han.q <han.q@wonhign>
2017-06-21 16:29:26 +0800

    自定制管理页面显示订单详情

--- orders/admin.py
+++ orders/admin.py
@@ -1,18 +1,56 @@
+import csv
+import datetime
+
 from django.contrib import admin
+from django.http import HttpResponse
+
 from .models import Order, OrderItem
 
+
+def export_to_csv(modeladmin, request, queryset):
+    """
+    导出csv文件
+    """
+    opts = modeladmin.model._meta
+    response = HttpResponse(content_type='text/csv')  # 告诉浏览器，文档是个CSV文件不是HTML文件
+    response['Content-Disposition'] = 'attachment;filename={}.csv'.format(opts.verbose_name)  # csv文件名
+    writer = csv.writer(response)
+    # writer.writerow(['First row', 'Foo', 'Bar', 'Baz'])
+    # writer.writerow(['Second row', 'A', 'B', 'C', '"Testing"', "Here's a quote"])
+
+    fields = [field for field in opts.get_fields() if not field.many_to_many and not field.one_to_many]
+    writer.writerow([field.verbose_name for field in fields])  # 第一行是标题
+    for obj in queryset:
+        data_row = []
+        for field in fields:
+            value = getattr(obj, field.name)
+            if isinstance(value, datetime.datetime):
+                value = value.strftime('%d/%m/%Y')
+            data_row.append(value)
+        writer.writerow(data_row)
+    return response
+export_to_csv.short_description = 'Export to CSV'
 
 class OrderItemInline(admin.TabularInline):
     model = OrderItem
     raw_id_fields = ['product']
 
+from django.urls import reverse
+def order_detail(obj):
+    """
+    订单详情
+    """
+    return '<a href="{}">订单详情</a>'.format(
+        reverse('orders:admin_order_detail', args=[obj.id]))
+order_detail.allow_tags = True   # 解释HTML标记
 
 class OrderAdmin(admin.ModelAdmin):
     list_display = ['id', 'first_name', 'last_name', 'email',
                     'address', 'postal_code', 'city', 'paid',
-                    'created', 'updated']
+                    'created', 'updated',order_detail]
     list_filter = ['paid', 'created', 'updated']
     inlines = [OrderItemInline]
+    actions = [export_to_csv]
 
 
 admin.site.register(Order, OrderAdmin)
--- orders/views.py
+++ orders/views.py
@@ -1,11 +1,24 @@
-from django.shortcuts import render
-from .models import Order
-from .models import OrderItem
-from .forms import OrderCreateForm
-from  cart.cart import Cart
-from .tasks import order_created
-from django.views.generic import View
+from django.shortcuts import render, HttpResponse
 
+from  cart.cart import Cart
+from .forms import OrderCreateForm
+from .models import OrderItem
+from .tasks import order_created
+from django.contrib.admin.views.decorators import staff_member_required
+from django.shortcuts import get_object_or_404
+from .models import Order
+from django.conf import settings
+from django.http import HttpResponse
+from django.template.loader import render_to_string
+
+
+@staff_member_required  # 检查用户请求这个页面的is_active以及is_staff字段是被设置为True
+def admin_order_detail(request, order_id):
+    """
+    扩展管理站点,定制一个订单详情页面
+    """
+    order = get_object_or_404(Order, id=order_id)
+    return render(request, 'orders/order/detail.html', locals())
 
 
 def order_create(request):
@@ -16,17 +29,12 @@ def order_create(request):
             order = form.save()
             for item in cart:
                 OrderItem.objects.create(order=order,
-                    product=item['product'],
-                    price=item['price'],
-                    quantity=item['quantity'])
-            cart.clear()   # 清楚购物车
+                                         product=item['product'],
+                                         price=item['price'],
+                                         quantity=item['quantity'])
+            cart.clear()  # 清楚购物车
             order_created.delay(order.id)  # 调用异步任务
-            return render(request,
-                'orders/order/created.html',
-                {'order': order})
+            return render(request, 'orders/order/created.html', locals())
     else:
         form = OrderCreateForm()
-    return render(request,
-            'orders/order/create.html',
-            {'cart': cart, 'form': form})
-
+    return render(request, 'orders/order/create.html', locals())
